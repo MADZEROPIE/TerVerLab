ee6f703e88c0f0bf940d301651f4ecf083da6e86
MADZEROPIE <ask_ii1@mail.ru>
2021-04-15 23:36:39 +0300

    almost done

--- lab_tv.py
+++ lab_tv.py
@@ -9,10 +9,12 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import math
 
-N = 10020005 #число экспериментов
+N = 1000009 #число экспериментов
 
 #lam=float(input("Введите параметр лямбда: "))
-lam=2
+lam=0
+while lam<1:
+    lam=float(input("Введите параметр лямбда: "))
 
 b=1-1/lam
 #print(b)
@@ -40,20 +42,20 @@ arr.sort()
 print()
 print()
 
-def M():
+def M() -> float:
     return 4/3*al*al-2*al*al+1/(lam*lam)
 
-def x_():
+def x_() -> float:
     sum=0
     for el in arr:
         sum+=el
     return sum/N
 
-def D():
+def D() -> float:
     M__=M()
     return 1/12*8*al*al*al+2/(lam*lam*lam) - M__*M__
 
-def S2():
+def S2() -> float:
     sum=0
     x__=x_()
     for el in arr:
@@ -71,13 +73,13 @@ print("D", D())
 print("|D - S2|", abs(S2()-D()))
 print()
 
-def Me():
+def Me() -> float:
     if N%2==0:
         return (arr[N//2-1]+arr[N//2])/2
     else:
         return arr[N//2]
     
-def R():
+def R() -> float:
     return arr[N-1]-arr[0]
 
         
@@ -93,15 +95,29 @@ print()
 #data['|D - S2|'] = abs(D() - S2())
 #print(data)
 
-def f(x:float):
+def f(x:float) -> float:
     if(x<0):
         return x/(2*al)+1
     else:
         return math.exp(-lam*x)
 
+def f1(x:float) -> float:
+    if(x>b):
+        return ((-1/lam) * math.log(lam*(1-x)))
+    else:
+        return 2*math.sqrt(al*x)-2*al
 
 
-z = [-1,  -0.7,  -0.5, -0.3, -0.15, -0.05, 0.0, 0.01,  0.1, 0.15, 0.16, 0.35,0.3501, 0.45, 0.55, 0.78,0.85, 1.0, 2.0, 3.0, 4, arr[N-1]]
+
+#z = [-1,  -0.7,  -0.5, -0.3, -0.15, -0.05, 0.0, 0.01,  0.1, 0.15, 0.16, 0.35,0.3501, 0.45, 0.55, 0.78,0.85, 1.0, 2.0, 3.0, 4, arr[N-1]]
+#k = len(z)
+
+k = int(N**(1/3))
+z=[0]*(k+1)
+for i in range(k):
+    z[i]=f1(i/k)
+z[-1]=arr[-1]
+
 k = len(z)
 
 z2 = [0]*(k-1)
@@ -125,8 +141,8 @@ for a in arr:
             break
 
         
-print(bins)
-print()
+#print(bins)
+#print()
 
 bins2=[0]*(k-1)
 for i in range(len(bins)):
@@ -136,9 +152,14 @@ fnj=[0]*(k-1)
 for i in range (k-1):
     fnj[i]=f(z2[i])
 
-print(z2)
-print(bins2)
-print(fnj)
+#print(z2)
+#print(bins2)
+#print(fnj)
+
+max_diff=0
+for i in range(len(bins2)):
+    max_diff=max(abs(bins2[i]-fnj[i]), max_diff)
+print("max_diff = ",max_diff)
 
 plt.figure(figsize=(10, 5))
 plt.step(z2, fnj, label='Theory')
@@ -148,7 +169,7 @@ plt.legend()
 plt.grid()
 plt.show()
 
-def qj(lb, rb):
+def qj(lb: float, rb: float) -> float:
     if lb<-2*b:
         lb=-2*b
     if lb<0 & rb>0:
@@ -167,11 +188,3 @@ def R0():
 
 alf=0.05
 
-masz=0
-for i in arr:
-    if(i>=1.0) & (i<=2.0):
-        masz+=1
-        
-print()
-print(masz/N)
-print(1/2*(math.exp(-2)-math.exp(-4)))
